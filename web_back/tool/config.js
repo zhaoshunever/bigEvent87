3bfebb94a4edf0552ca2382f12c2eb9a134b2c76
zhao <1924502319@qq.com>
2019-10-13 10:58:02 +0800

    用户登录

---
+++ web_back/tool/config.js
@@ -0,0 +1,7 @@
+//config - 中文含义为配置
+//将项目中使用的地址统一保存在这个文件中
+var baseUrl = ' http://localhost:8000/';
+//用户登录
+var USER_LOGIN = baseUrl + 'admin/login';
+//用户退出
+var USER_LOGOUT = baseUrl + 'admin/logout';
